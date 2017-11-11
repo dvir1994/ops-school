5fc5bae469470248ed8c9871977b7cf5bfc5b292
dvir1994 <33489902+dvir1994@users.noreply.github.com>
2017-11-11 18:47:35 +0200

    Update exercise2-fix.sh

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise2 here
+#last line in /etc/hosts contains "127.0.0.1 www.ascii-art.de"
+#delete the last line in /etc/hosts
+sudo sed -i '$ d' /etc/hosts
+
+#option 2
+#sudo head -n -1 /etc/hosts > temp.txt ; mv temp.txt /etc/hosts
