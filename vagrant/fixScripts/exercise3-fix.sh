70a6eba899f7a43ab8a8c56abf3b72e7a2476b99
dvir1994 <33489902+dvir1994@users.noreply.github.com>
2017-11-11 18:48:46 +0200

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise3 here
+#server is apache2 with no permission for end user to view the web page
+#change the line 'deny from all' at /etc/apache2/sites-enabled/000-default under '/var/wwww' section
+sudo sed -i 's/deny from all/Allow from all/' /etc/apache2/sites-enabled/000-default
+sudo service apache2 restart
