6d68d61f0f44605e513828a2af2b72414add6d74
dvir1994 <33489902+dvir1994@users.noreply.github.com>
2017-11-11 18:49:59 +0200

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -t rsa
+#our ssh request is signed using server1 private key, we will copy server1 public key to server2 so we wont be prompted for password
+#we should take the public key (id_rsa.pub) and echo it to server2 authorized keys file (authorized_keys)
+ssh-copy-id server2
